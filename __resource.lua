15dac5933aa1a760c21ba7b58d174f83709710d7
FAXES <31368070+FAXES@users.noreply.github.com>
2019-02-03 09:44:11 +1100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,6 @@
+----------------------------------------
+--- Discord Whitelist, Made by FAXES ---
+----------------------------------------
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+server_script "server.lua"
---
+++ server.lua
@@ -0,0 +1,35 @@
+----------------------------------------
+--- Discord Whitelist, Made by FAXES ---
+----------------------------------------
+
+--- Config ---
+roleNeeded = "Member" -- The role nickname needed to pass the whitelist
+notWhitelisted = "You are not whitelisted for this server." -- Message displayed when they are not whitelist with the role
+noDiscord = "You must have Discord open to join this server." -- Message displayed when discord is not found
+
+
+--- Code ---
+
+AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
+    local src = source
+    deferrals.defer()
+    deferrals.update("Checking Permissions")
+
+    for k, v in ipairs(GetPlayerIdentifiers(src)) do
+        if string.sub(v, 1, string.len("discord:")) == "discord:" then
+            identifierDiscord = v
+        end
+    end
+
+    local allowed = false
+    if identifierDiscord then
+        if exports.discord_perms:IsRolePresent(src, "Member") then
+            deferrals.done()
+            allowed = true
+        else
+            deferrals.done(notWhitelisted)
+        end
+    else
+        deferrals.done(noDiscord)
+    end
+end)
