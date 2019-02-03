424bac98cae5e3bb06d9b55f576d3252a80ad23f
Jarrett B <itsJarrett@users.noreply.github.com>
2019-02-02 17:14:23 -0800

    Update __resource.lua
    add dependency

--- __resource.lua
+++ __resource.lua
@@ -2,5 +2,6 @@
 --- Discord Whitelist, Made by FAXES ---
 ----------------------------------------
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+dependency 'discord_perms'
 
 server_script "server.lua"
